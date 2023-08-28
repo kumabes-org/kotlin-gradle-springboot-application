b213f2054cc74166260745c50cb1bfb08305a450
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-08-28 18:26:11 -0300

    update plugin.spring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ import org.gradle.kotlin.dsl.*
 plugins {
 	id("java")
 	id("jacoco")
-	id("org.sonarqube") version "4.0.0.2929"
+	id("org.sonarqube") version "4.3.0.3225"
 	id("org.springframework.boot") version "2.7.10-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.2"
-	kotlin("jvm") version "1.9.1"
-	kotlin("plugin.spring") version "1.9.1"
-	kotlin("plugin.jpa") version "1.9.1"
+	id("io.spring.dependency-management") version "1.1.3"
+	kotlin("jvm") version "1.9.10"
+	kotlin("plugin.spring") version "1.9.10"
+	kotlin("plugin.jpa") version "1.9.10"
 }
 
 group = "br.com.kumabe"
